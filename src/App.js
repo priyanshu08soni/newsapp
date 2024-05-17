3ad7043c3da8de264290f6bb66222c7cb49d89d1
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-17 08:03:28 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -21,14 +21,14 @@ const App=()=> {
           <Navbar/>
           <LoadingBar color='#f11946' height={3} progress={progress}/>
               <Routes>
-                <Route exact path="" element={<News setProgress={setProgress} apiKey={apiKey}   key="general" pageSize={pageSize} country="in" category="general"/>}></Route>
-                <Route exact path="/bussness" element={<News setProgress={setProgress} apiKey={apiKey}   key="bussness" pageSize={pageSize} country="in" category="bussness"/>}></Route>
-                <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey}   key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}></Route>
-                <Route exact path="/general" element={<News setProgress={setProgress} apiKey={apiKey}   key="general" pageSize={pageSize} country="in" category="general"/>}></Route>
-                <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey}   key="health" pageSize={pageSize} country="in" category="health"/>}></Route>
-                <Route exact path="/science" element={<News setProgress={setProgress} apiKey={apiKey}   key="science" pageSize={pageSize} country="in" category="science"/>}></Route>
-                <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey}   key="sports" pageSize={pageSize} country="in" category="sports"/>}></Route>
-                <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey}   key="technology" pageSize={pageSize} country="in" category="technology"/>}></Route>
+                <Route exact path="/" element={<News setProgress={setProgress}   key="general" pageSize={pageSize} country="in" category="general"/>}></Route>
+                <Route exact path="/bussness" element={<News setProgress={setProgress}   key="bussness" pageSize={pageSize} country="in" category="bussness"/>}></Route>
+                <Route exact path="/entertainment" element={<News setProgress={setProgress}   key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}></Route>
+                <Route exact path="/general" element={<News setProgress={setProgress}   key="general" pageSize={pageSize} country="in" category="general"/>}></Route>
+                <Route exact path="/health" element={<News setProgress={setProgress}   key="health" pageSize={pageSize} country="in" category="health"/>}></Route>
+                <Route exact path="/science" element={<News setProgress={setProgress}   key="science" pageSize={pageSize} country="in" category="science"/>}></Route>
+                <Route exact path="/sports" element={<News setProgress={setProgress}   key="sports" pageSize={pageSize} country="in" category="sports"/>}></Route>
+                <Route exact path="/technology" element={<News setProgress={setProgress}   key="technology" pageSize={pageSize} country="in" category="technology"/>}></Route>
               </Routes>
         </Router>
       </div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -15,7 +15,7 @@ const News=(props)=> {
   }
   const updateNews=async ()=>{
     props.setProgress(10);
-    const url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    const url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=651a7a002d4048b39da8d678e208ad84&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
     let data=await fetch(url);
     props.setProgress(30);
